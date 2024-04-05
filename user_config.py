5fa4550b388fa876350b3b6006afb49db001936a
iamcaiyu <iamcaiyu@gmail.com>
2024-04-05 10:40:53 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,10 @@
+source_file = "demo.txt"
+final_file = "result.txt"
+favorite_list = []
+favorite_page_num = 6
+default_page_num = 4
+urls_limit = 15
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "all"
